4b5f7dd23b28321ff2169bbfe3ec419c89a98035
tarkanbatar <tarkanbatar4@gmail.com>
2023-02-17 23:18:22 +0300

    feat: getAll endpoint added

--- routes/movies.js
+++ routes/movies.js
@@ -4,6 +4,20 @@ const router = express.Router();
 //* Models
 const Movie = require('../models/Movie');
 
+router.get('/',(req,res)=>{  //Butun filmleri getiren endpoint
+  const promise = Movie.find({ });
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+router.get('/:movie_id', (req,res)=>{       //URL'de gonderilen id numarasi req.params degiskenine dusecek ve biz bu bilgiye oradan ulasip kullanacagiz.
+
+});
+
+
 router.post('/', function(req, res, next) {
 /*const { title, imdb_score, category, country, year } = req.body;
 
