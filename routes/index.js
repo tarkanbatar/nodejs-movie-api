6abf1b5f7c97c435e627ab40caee3e7adf96e87b
tarkanbatar <tarkanbatar4@gmail.com>
2023-02-19 22:43:20 +0300

    feat:register endpoint and password hashing added, compare will be added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+//* Models
+const User = require('../models/User');
+
+//* Bcrypt importation
+const bcrypt = require('bcryptjs'); 
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', function(req, res, next) {
+  const { username, password} = req.body;
+
+  bcrypt.hash(password, 10).then((hash)=>{
+    const user = new User({
+      username,   //? username=this.username yapmak yerine direkt bu sekilde kullanabiliyoruz
+      password: hash    //! bu hashlenmis sifrenin cozulmesi icin de compare methodu kullanilir bcryptjs compare diye aratirsak nasil kullanildigi gorulecektir.
+    });
+  
+    const promise = user.save();
+    promise.then((data)=>{
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    });
+
+  });
+
+
 });
 
 module.exports = router;
